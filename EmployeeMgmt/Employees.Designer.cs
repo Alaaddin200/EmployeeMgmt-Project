f01dcc80b9e67e996a7df7e5624b9a40e7d88553
elsafl22 <ahmed.alaaeldine21@gmail.com>
2023-01-23 07:16:42 +0200

    add manage employee text

--- EmployeeMgmt/Employees.Designer.cs
+++ EmployeeMgmt/Employees.Designer.cs
@@ -30,6 +30,7 @@
         {
             this.panel1 = new System.Windows.Forms.Panel();
             this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
             this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -53,12 +54,24 @@
             this.label1.TabIndex = 1;
             this.label1.Text = "Employee Management System Version 1.0";
             // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label2.ForeColor = System.Drawing.Color.Teal;
+            this.label2.Location = new System.Drawing.Point(742, 145);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(195, 28);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Manage Employees";
+            // 
             // Employees
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 28F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
             this.ClientSize = new System.Drawing.Size(1388, 796);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.panel1);
             this.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
@@ -68,6 +81,7 @@
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -75,5 +89,6 @@
 
         private Panel panel1;
         private Label label1;
+        private Label label2;
     }
 }
