26017a3866afa16edeb384af43c0c7a275d6b07d
elsafl22 <ahmed.alaaeldine21@gmail.com>
2023-01-23 09:08:30 +0200

    start adding data base

--- EmployeeMgmt/Functions.cs
+++ EmployeeMgmt/Functions.cs
@@ -3,11 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
 
 namespace EmployeeMgmt
 {
     internal class Functions
     {
+        private SqlConnection Con;
+        private SqlCommand Cmd;
+        private DataTable dt;
+        private SqlDataAdapter sda;
+        private string ConStr;
+
         public Functions() { 
             
         }
