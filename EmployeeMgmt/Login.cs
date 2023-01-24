d8f7a334052d278ec25ba398c925a36e4776b6a1
elsafl22 <ahmed.alaaeldine21@gmail.com>
2023-01-24 11:23:24 +0200

    add method to on login button

--- EmployeeMgmt/Login.cs
+++ EmployeeMgmt/Login.cs
@@ -12,5 +12,24 @@ namespace EmployeeMgmt
             UNameTb.Text = "";
             PasswordTb.Text = "";
         }
+
+        private void LoginBtn_Click(object sender, EventArgs e)
+        {
+            if (UNameTb.Text == "" || PasswordTb.Text == "")
+            {
+                MessageBox.Show("Missing Data!!");
+            }
+            else if (UNameTb.Text == "Admin" && PasswordTb.Text == "password")
+            {
+                Employees Obj = new Employees();
+                Obj.Show();
+                this.Hide();
+            }
+            else {
+                MessageBox.Show("Wrong UserName Or Password!!");
+                UNameTb.Text = "";
+                PasswordTb.Text = "";
+            }
+        }
     }
 }
